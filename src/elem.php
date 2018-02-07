b16dbb14cb4187e109ece010fdef83a80aa7300c
Marina Duca <marinaalexandraduca06@gmail.com>
2018-02-07 02:33:12 +0200

    Initial commit

---
+++ src/elem.php
@@ -0,0 +1,21 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+$app->get('/api/elem', function(Request $request, Response $response){
+  $sql = "SELECT * FROM test";
+  try{
+      // Get DB Object
+      $db = new db();
+      // Connect
+      $db = $db->connect();
+      $stmt = $db->query($sql);
+      $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+      $db = null;
+      echo json_encode($customers);
+  } catch(PDOException $e){
+      echo '{"error": {"text": '.$e->getMessage().'}';
+  }
+});
