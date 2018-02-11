97238bfbf1d1f2639cd674c6ee802eb604cd21c6
Marina Duca <marinaalexandraduca06@gmail.com>
2018-02-11 03:58:54 +0200

    Deploy experiment

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
-require '../src/config/db.php';
+require './vendor/autoload.php';
+require './src/config/db.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
@@ -13,6 +13,6 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response;
 });
 
-require '../src/elem.php';
+require './src/elem.php';
 
 $app->run();
--- src/elem.php
+++ src/elem.php
@@ -19,3 +19,20 @@ $app->get('/api/elem', function(Request $request, Response $response){
       echo '{"error": {"text": '.$e->getMessage().'}';
   }
 });
+
+$app->get('/api/elem/{id}', function(Request $request, Response $response){
+  $id = $request->getAttribute('id');
+  $sql = "SELECT * FROM test WHERE id = $id";
+  try{
+      // Get DB Object
+      $db = new db();
+      // Connect
+      $db = $db->connect();
+      $stmt = $db->query($sql);
+      $customer = $stmt->fetch(PDO::FETCH_OBJ);
+      $db = null;
+      echo json_encode($customer);
+  } catch(PDOException $e){
+      echo '{"error": {"text": '.$e->getMessage().'}';
+  }
+});
